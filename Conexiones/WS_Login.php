382aa9c7170a6720b0572eb15c2eb5d37e09feac
anthonyca18m <anthonyca18m@gmail.com>
2019-11-11 01:02:22 -0500

    modificando el WS_Login con la new DB readyStatus = 3

--- Conexiones/WS_Login.php
+++ Conexiones/WS_Login.php
@@ -3,36 +3,35 @@
 require_once("Config.php");
 
 
-$json=array();
+$json = array();
 
 	if(isset($_GET["Ucorreo"]) && isset($_GET["Upass"])){
 		
 		$Ucorreo = htmlentities( $_GET['Ucorreo']);
 		$Upass   = htmlentities( $_GET['Upass']);
 		
-		$conexion=mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+		$conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 		
 		$consulta="SELECT * FROM usuario WHERE Ucorreo = '{$Ucorreo}' AND Upass = '{$Upass}'";
 		$resultado=mysqli_query($conexion,$consulta);
+		$count=mysqli_query($conexion,$consulta);
 
-		if($consulta){
+		if(mysqli_fetch_row($count) >= 1){
 		
-			if($reg=mysqli_fetch_array($resultado)){
-				$json['datos'][]=$reg;
+			if($rs = mysqli_fetch_array($resultado)){
+				$json['datos'][] = $rs ;
 			}
 			mysqli_close($conexion);
 			echo json_encode($json);
 		}else{
-			$results["Ucorreo"]='No encontrado';
-			$results["Upass"]='No encontrado';
-			$json['datos'][]=$results;
-			echo json_encode($json);
-		}
-		
+				$results["Ucorreo"]='No encontrado';
+				$results["Upass"]='No encontrado';
+				$json['datos'][]=$results;
+				echo json_encode($json);
+			}
 	}else{
-		   	$results["Ucorreo"]='WS Registro no encontrado!';
-			$results["Upass"]='WS Registro no encontrado!';
-			$json['datos'][]=$results;
-			echo json_encode($json);
-		}
-?>
+	   	$results["Ucorreo"]='WS Registro no encontrado!';
+		$results["Upass"]='WS Registro no encontrado!';
+		$json['datos'][]=$results;
+		echo json_encode($json);
+	}
