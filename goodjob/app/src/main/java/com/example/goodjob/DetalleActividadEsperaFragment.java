cc8f92f0d0e1dd9733152d8bb3515e64e92ab061
anthonyca18m <anthonyca18m@gmail.com>
2019-11-23 15:02:25 -0500

    design de lsitado de actividades con gestion actividades

---
+++ goodjob/app/src/main/java/com/example/goodjob/DetalleActividadEsperaFragment.java
@@ -0,0 +1,29 @@
+package com.example.goodjob;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetalleActividadEsperaFragment extends Fragment {
+
+
+    public DetalleActividadEsperaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detalle_actividad_espera, container, false);
+    }
+
+}
---
+++ goodjob/app/src/main/java/com/example/goodjob/ListarActividadesEsperaFragment.java
@@ -0,0 +1,39 @@
+package com.example.goodjob;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListarActividadesEsperaFragment extends Fragment {
+
+
+    public ListarActividadesEsperaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_listar_actividades_espera, container, false);
+
+
+
+
+
+
+
+
+
+        return view;
+    }
+
+}
